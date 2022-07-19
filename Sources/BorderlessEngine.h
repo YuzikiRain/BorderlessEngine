976b93b8ef6fa8a35438660dead3940cfc8e3e4f
覃雨 <1404094167@qq.com>
2022-07-19 08:42:22 +0800

    find library

--- Sources/BorderlessEngine.h
+++ Sources/BorderlessEngine.h
@@ -1,4 +1,23 @@
-namespace BorderlessEngine
-{
+#pragma once
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include <list>
+
+namespace BorderlessEngine {
     void Launch();
+	bool InitializeWindow();
+	void InitSystems();
+	void GameLoop();
+	void Quit();
+	bool ShouldQuit();
+	void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+	double GetFrameInterval();
+	double GetFrameRate();
+
+	extern double targetFrameRate;
+	extern double realTime;
+	extern double frameTime;
+	extern double accumulator;
+	extern double timeScale;
+
 }
