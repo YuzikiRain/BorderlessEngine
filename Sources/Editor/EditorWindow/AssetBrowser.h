8ae7f4a89f6c3ba7b88877db6191972ae012b108
覃雨 <1404094167@qq.com>
2023-03-02 18:53:35 +0800

    drag & drop

--- Sources/Editor/EditorWindow/AssetBrowser.h
+++ Sources/Editor/EditorWindow/AssetBrowser.h
@@ -26,7 +26,7 @@ namespace BorderlessEditor
         {
             isOpen = true;
             assetPath = AssetDatabase::GetAssetPath();
-            m_CurrentDirectory = std::filesystem::path(assetPath);
+            currentDirectory = std::filesystem::path(assetPath);
 
             OleInitialize(NULL);
             dropManager = DropManager();
@@ -39,31 +39,38 @@ namespace BorderlessEditor
             ImGui::Begin("AssetBrowser");
 
             // 拖拽
-            //     // Using a Child allow to fill all the space of the window.
-            //     // It also alows customization
-            //     ImGui::BeginChild("AssetBrowser");
-            //     // Get the size of the child (i.e. the whole draw size of the windows).
+            // Using a Child allow to fill all the space of the window.
+            // It also alows customization
+            ImGui::BeginChild("AssetBrowser");
+            // Get the size of the child (i.e. the whole draw size of the windows).
 
-            //     if (dropManager.IsDragging()) // somehow set to true when dragging files on top of your application
-            //     {
-            //         if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_SourceExtern)) // we use an external source (i.e. not ImGui-created)
-            //         {
-            //             // replace "FILES" with whatever identifier you want - possibly dependant upon what type of files are being dragged
-            //             // you can specify a payload here with parameter 2 and the sizeof(parameter) for parameter 3.
-            //             // I store the payload within a vector of strings within the application itself so don't need it.
-            //             ImGui::SetDragDropPayload("FILES", nullptr, 0);
-            //             ImGui::BeginTooltip();
-            //             ImGui::Text("FILES");
-            //             ImGui::EndTooltip();
-            //             ImGui::EndDragDropSource();
-            //         }
-            //     }
+            if (dropManager.IsDragging()) // somehow set to true when dragging files on top of your application
+            {
+                if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_SourceExtern)) // we use an external source (i.e. not ImGui-created)
+                {
+                    // replace "FILES" with whatever identifier you want - possibly dependant upon what type of files are being dragged
+                    // you can specify a payload here with parameter 2 and the sizeof(parameter) for parameter 3.
+                    // I store the payload within a vector of strings within the application itself so don't need it.
+                    ImGui::SetDragDropPayload("FILES", nullptr, 0);
+                    ImGui::BeginTooltip();
+                    ImGui::Text("FILES");
+                    ImGui::EndTooltip();
+                    ImGui::EndDragDropSource();
+                }
+            }
 
-            //     // // Because I use the texture from OpenGL, I need to invert the V from the UV.
-            //     // ImGui::Image((ImTextureID)texture, wsize, ImVec2(0, 1), ImVec2(1, 0));
+            if (ImGui::BeginDragDropTarget())
+            {
+                if (const ImGuiPayload *payload = ImGui::AcceptDragDropPayload("FILES"))
+                {
+                    auto d = payload->Data;
+                }
+            }
 
-            //     ImGui::EndChild();
-            // }
+            // // Because I use the texture from OpenGL, I need to invert the V from the UV.
+            // ImGui::Image((ImTextureID)texture, wsize, ImVec2(0, 1), ImVec2(1, 0));
+
+            ImGui::EndChild();
 
             static float padding = 16;
             static float thumbnailSize = 128;
@@ -75,18 +82,18 @@ namespace BorderlessEditor
             ImGui::SliderFloat("Thumbnail Size", &thumbnailSize, 16, 128);
             ImGui::SliderFloat("padding", &padding, 0, 32);
 
-            auto relativePath = std::filesystem::relative(m_CurrentDirectory, std::filesystem::path(assetPath).parent_path());
+            auto relativePath = std::filesystem::relative(currentDirectory, std::filesystem::path(assetPath).parent_path());
             ImGui::Text(relativePath.string().c_str());
             // 不是资源目录（根目录），则显示回到上一级按钮
-            if (m_CurrentDirectory != std::filesystem::path(assetPath))
+            if (currentDirectory != std::filesystem::path(assetPath))
             {
                 if (ImGui::Button("<-"))
-                    m_CurrentDirectory = m_CurrentDirectory.parent_path();
+                    currentDirectory = currentDirectory.parent_path();
             }
 
             // 显示当前目录
             ImGui::Columns(columnCount, 0, false);
-            for (auto &directoryEntry : std::filesystem::directory_iterator(m_CurrentDirectory))
+            for (auto &directoryEntry : std::filesystem::directory_iterator(currentDirectory))
             {
                 const auto &path = directoryEntry.path();
                 auto relativePath = std::filesystem::relative(path, assetPath);
@@ -96,7 +103,7 @@ namespace BorderlessEditor
                 if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left))
                 {
                     if (directoryEntry.is_directory())
-                        m_CurrentDirectory /= path.filename();
+                        currentDirectory /= path.filename();
                 }
                 ImGui::Text(filenameString.c_str());
 
@@ -119,6 +126,6 @@ namespace BorderlessEditor
         HWND hwnd;
         DropManager dropManager;
         std::string assetPath;
-        std::filesystem::path m_CurrentDirectory;
+        std::filesystem::path currentDirectory;
     };
 }
